9d1f11dffeafeb5b7450d95b71c878f15ccd8765
Based <basedjamali@gmail.com>
2023-02-25 22:54:35 +0100

    Updated video_downloader.py

--- Youtube Video Downloader/video_downloader.py
+++ Youtube Video Downloader/video_downloader.py
@@ -6,7 +6,7 @@ mp4_file = yt.streams.filter(file_extension="mp4")
 video_resolution = mp4_file.get_highest_resolution()
 
 try:
-  video_resolution.download(r"C:\Users\based\Videos\YouTube Videos")
+  video_resolution.download(r"Name of the file path")
   print("The video has been successfully downloaded!")
 except:
   print("An error has been occurred.")
